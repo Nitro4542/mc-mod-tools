f32d4e16d7e48f216dd8d579a4a72531feca9dc8
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2023-05-21 14:06:47 +0200

    Start of this project
    You can now read the info.txt in your modpack

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# mc-mod-installer
-Installer for my Minecraft mod format
+# About this project
+
+This is an installer for my Minecraft mod format.
+I've created this project for people who don't know how to install mods and for those, who don't wan't to use some third-party launcher and wan't something safe, ad and crap-free tool.
+
+## How does it work?
+
+## How do I create mod packs for this?
+
+I'm going to create a tool for that. For now you'll have to do it manually.
+
+### File format/structure explained
+
+As this is the Alpha, you can currently only create an info.txt and read it. Later on, you can paste mods.
---
+++ installer-cmd/get-modpack-info-cmd.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+from zipfile import ZipFile
+
+# Arguments
+parser = argparse.ArgumentParser(prog='nitro-mc-mod-info',description='Get modpack info from mods.zip format')
+parser.add_argument("-f", dest = "file", help="location of mods.zip")
+args = parser.parse_args()
+
+# Create folders
+if os.path.isdir("%temp%\\nitro-mc-mod-installer") != True:
+    os.mkdir("%temp%\\nitro-mc-mod-installer")
+if os.path.isdir("%temp%\\nitro-mc-mod-installer\\info") != True:
+    os.mkdir("%temp%\\nitro-mc-mod-installer\\info")
+
+# Check if modpack is specified
+if args.file is not None:
+# Extract info.txt from modpack
+    with ZipFile(args.file, 'r') as zipobject: 
+        zipobject.extract(
+            "info.txt", path="%temp%\\nitro-mc-mod-installer\\info")
+    zipobject.close()
+    open("%temp%\\nitro-mc-mod-installer\\info\\info.txt")

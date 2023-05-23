45f9c69627d8cd1f86485a8c58c9608c702c273c
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2023-05-23 17:17:02 +0200

    Minecraft mod directory can now be changed

--- scripts/backup-mods.py
+++ scripts/backup-mods.py
@@ -11,13 +11,19 @@ config.read('config.ini')
 # Arguments
 parser = argparse.ArgumentParser(prog='nitro-mc-mod-backup',description='Backup current Minecraft mods')
 parser.add_argument("-d", dest = "destination", help="backup destination (will use path from config.ini if not given)", required=False, default=None)
+parser.add_argument("-md", dest = "mc_directory", help="minecraft directory (will use path from config.ini if not given)", required=False, default=None)
 args = parser.parse_args()
 
-# Check configuration if -d isn't used
+# Check configuration if -d or -md aren't used
 if args.destination != None:
     if config.get('Paths','default-backup-path') != "NOT_SET":
         if os.path.isdir(config.get('Paths','default-backup-path')) != True:
-            print("The path in the configuration isn't valid or doesn't exist.")
+            print("The backup path in the configuration isn't valid or doesn't exist.")
+            quit()
+if args.mc_directory != None:
+    if config.get('Paths','minecraft-mod-folder') != "default":
+        if os.path.isdir(config.get('Paths','minecraft-mod-folder')) != True:
+            print("The minecraft mod folder path in the configuration isn't valid or doesn't exist.")
             quit()
 
 # Get and add backup name to destination and create folder if neccessary
@@ -28,8 +34,15 @@ else:
 if os.path.isdir(final_destination) != True:
         os.mkdir(final_destination)
 
+# Set mod folder path
+if args.mc_directory != None:
+    src = args.mc_directory
+elif config.get('Paths','minecraft-mod-folder') != "default":
+    src = config.get('Paths','minecraft-mod-folder')
+else:
+    src = os.getenv('APPDATA')+"\\.minecraft\\mods"
+
 # Copy all content of mods folder into backup destination
-src = os.getenv('APPDATA')+"\\.minecraft\\mods"
 src_files = os.listdir(src)
 for file_name in src_files:
     full_file_name = os.path.join(src, file_name)
--- scripts/install-mods.py
+++ scripts/install-mods.py
@@ -1,15 +1,38 @@
 import argparse
 import os
 import shutil
+from configparser import ConfigParser
+
+# Load configuration
+config = ConfigParser()
+config.read('config.ini')
 
 # Arguments
 parser = argparse.ArgumentParser(prog='nitro-mc-mod-installer',description='Instantly install a whole Minecraft mod pack')
-parser.add_argument("-d", dest = "destination", help="source folder destination",required=True)
+parser.add_argument("-d", dest = "destination", help="source folder destination",required=True, default=None)
+parser.add_argument("-md", dest = "mc_directory", help="minecraft directory (will use path from config.ini if not given)", required=False, default=None)
 args = parser.parse_args()
 
-# Copy all content of selected folder into mods folder
+# Check configuration if -d or -md aren't used
+if args.destination != None:
+    if config.get('Paths','default-backup-path') != "NOT_SET":
+        if os.path.isdir(config.get('Paths','default-backup-path')) != True:
+            print("The backup path in the configuration isn't valid or doesn't exist.")
+            quit()
+if args.mc_directory != None:
+    if config.get('Paths','minecraft-mod-folder') != "default":
+        if os.path.isdir(config.get('Paths','minecraft-mod-folder')) != True:
+            print("The minecraft mod folder path in the configuration isn't valid or doesn't exist.")
+            quit()
+
+# Set mod and source mod pack folder path
 src = args.destination
-final_destination = os.getenv('APPDATA')+"\\.minecraft\\mods"
+if config.get('Paths','minecraft-mod-folder') != "default":
+    final_destination = config.get('Paths','minecraft-mod-folder')
+else:
+    final_destination = os.getenv('APPDATA')+"\\.minecraft\\mods"
+
+# Copy all content of selected folder into mods folder
 src_files = os.listdir(src)
 for file_name in src_files:
     full_file_name = os.path.join(src, file_name)

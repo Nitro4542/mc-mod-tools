5acdbdfa9a7408c02c74d21e64024ae3fd6b83ce
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2023-07-06 17:02:03 +0200

    Fixed formatting

--- scripts/mc_mod_gui.py
+++ scripts/mc_mod_gui.py
@@ -6,7 +6,7 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.textinput import TextInput  #
+from kivy.uix.textinput import TextInput
 
 # Load configuration
 # config = ConfigParser()

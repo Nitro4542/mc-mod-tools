c1168f51e0ffb544a375694e53a1cfde75c7bd9e
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2023-09-22 16:17:04 +0200

    Massive improvements

--- scripts/mc_mod_tools.py
+++ scripts/mc_mod_tools.py
@@ -59,43 +59,19 @@ if __name__ == "__main__":
             print("The backup path given isn't valid or doesn't exist.")
             sys.exit(1)
 
-# Check configuration
-if args.destination is None:
-    if config.get('Paths', 'default-backup-path') != "NOT_SET":
-        if not os.path.isdir(config.get('Paths', 'default-backup-path')):
-            print("The backup path in the configuration isn't valid or doesn't exist.")
-            sys.exit(1)
-    elif config.get('Paths', 'default-backup-path') == "NOT_SET":
-        print("The backup path in the configuration isn't given.")
-        sys.exit(1)
-if args.mc_directory is None:
-    if config.get('Paths', 'minecraft-mod-folder') != "default":
-        if not os.path.isdir(config.get('Paths', 'minecraft-mod-folder')):
-            print(
-                "The minecraft mod folder path in the configuration isn't valid or doesn't exist.")
-            sys.exit(1)
-if args.cache_folder is None:
-    if config.get('Paths', 'cache-folder') != "NOT_SET":
-        if not os.path.isdir(config.get('Paths', 'cache-folder')):
-            print("The cache path in the configuration isn't given.")
-            sys.exit(1)
-    elif config.get('Paths', 'cache-folder') == "NOT_SET":
-        print("The cache path in the configuration isn't given.")
-        sys.exit(1)
+
+def randomword(length):
+    """Generates a random string
+    Needed for some functions to work properly"""
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(length))
 
 
-# @Nitro4542 pls fix
-# if config.get('General','operating-system') != "Windows":
-#    print('Please check your configuration at operating-system.')
-#    sys.exit(1)
-# elif config.get('General','operating-system') != "Linux":
-#    print('Please check your configuration at operating-system.')
-#    sys.exit(1)
-
-
-def set_mod_folder():
+def get_mod_folder(modfolder_input):
     """Sets mod folder path"""
-    if args.mc_directory is not None:
+    if modfolder_input is not None:
+        mod_folder = modfolder_input
+    elif args.mc_directory is not None:
         mod_folder = args.mc_directory
     elif config.get('Paths', 'minecraft-mod-folder') != "default":
         mod_folder = config.get('Paths', 'minecraft-mod-folder')
@@ -107,11 +83,32 @@ def set_mod_folder():
     return mod_folder
 
 
-def randomword(length):
-    """Generates a random string
-    Needed for some functions to work properly"""
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(length))
+def get_backup_folder(backupfolder_input):
+    """Sets backup folder"""
+    if config.get('Paths', 'default-backup-path') == "default":
+        backup_path = "Backups"
+    else:
+        if backupfolder_input is not None:
+            backup_path = backupfolder_input
+        elif args.destination is not None:
+            backup_path = args.destination
+        elif config.get('Paths', 'default-backup-path') != "default":
+            backup_path = config.get('Paths', 'default-backup-path')
+    return backup_path
+
+
+def get_cache_folder(cachefolder_input):
+    """Sets cache folder"""
+    if config.get('Paths', 'cache-folder') == "default":
+        cache_path = "Cache"
+    else:
+        if cachefolder_input is not None:
+            cache_path = cachefolder_input
+        elif args.cache_folder is not None:
+            cache_path = args.cache_folder
+        elif config.get('Paths', 'cache_folder') != "default":
+            cache_path = config.get('Paths', 'cache_folder')
+    return cache_path
 
 
 def unzip_mod_pack(cache_dest, zipfile_path):
@@ -121,20 +118,11 @@ def unzip_mod_pack(cache_dest, zipfile_path):
         if os.path.isdir(cache_dest):
             final_destination = (cache_dest + "\\" + datetime.today().strftime('%Y-%m-%d') + "_" +
                                  randomword(5))
-    elif args.cache_folder is None:
-        if os.path.isdir(config.get('Paths', 'cache-folder')):
-            final_destination = (config.get('Paths', 'cache-folder') + "\\" +
-                                 datetime.today().strftime('%Y-%m-%d') + "_" + randomword(5))
-    elif args.cache_folder is not None:
-        if os.path.isdir(args.cache_folder):
-            final_destination = args.cache_folder + "\\" + datetime.today().strftime(
-                '%Y-%m-%d') + "_" + randomword(5)
-        else:
-            print("The cache path isn't valid or doesn't exist.")
-            sys.exit(1)
     else:
-        print('Error: Please provide a valid cache folder path.')
-        sys.exit(1)
+        final_destination = (get_cache_folder(None) + "\\" + datetime.today().strftime('%Y-%m-%d')
+                             + "_" + randomword(5))
+    if not os.path.isdir(final_destination):
+        os.mkdir(final_destination)
     # Extract zip file
     if cache_dest is not None:
         with ZipFile(zipfile_path, 'r') as zipobject:
@@ -151,8 +139,8 @@ def unzip_mod_pack(cache_dest, zipfile_path):
 
 def create_backup(backup_dest):
     """Creates a backup of your mods folder"""
-    mc_mod_folder = set_mod_folder()
-    # Create folder for zip file + Check configuration
+    mc_mod_folder = get_mod_folder(None)
+    # Create folder for backup
     if backup_dest is not None:
         if os.path.isdir(backup_dest):
             final_destination = (backup_dest + "\\" + datetime.today().strftime('%Y-%m-%d') + "_" +
@@ -161,12 +149,8 @@ def create_backup(backup_dest):
             print("The backup path given isn't valid or doesn't exist.")
             sys.exit(1)
     # Get and add backup name to destination and create folder if necessary
-    elif config.get('Paths', 'default-backup-path') == "NOT_SET":
-        final_destination = args.destination + "\\" + "mod-backup-" + datetime.today().strftime(
-            '%Y-%m-%d') + "_" + randomword(5)
     else:
-        final_destination = (config.get('Paths',
-                                        'default-backup-path') + "\\" + "mod-backup-" +
+        final_destination = (get_backup_folder(None) + "\\" + "mod-backup-" +
                              datetime.today().strftime('%Y-%m-%d') + "_" + randomword(5))
     if not os.path.isdir(final_destination):
         os.mkdir(final_destination)
@@ -180,7 +164,7 @@ def create_backup(backup_dest):
 
 def delete_mods():
     """Deletes all mods in mods folder"""
-    mc_mod_folder = set_mod_folder()
+    mc_mod_folder = get_mod_folder(None)
     for filename in os.listdir(mc_mod_folder):
         file_path = os.path.join(mc_mod_folder, filename)
         try:
@@ -192,7 +176,7 @@ def delete_mods():
             print("An exception occurred")
 
 
-def confirm_prompt():
+def confirmation_prompt():
     """Confirmation prompt for delete_mods()"""
     answer0 = input("Do you want to create a backup of your existing mods? [Y|N]")
     if answer0.lower() in ["y", "yes"]:
@@ -201,7 +185,7 @@ def confirm_prompt():
         print('Won\'t create backup')
     else:
         print('Only \"y\" or \"n\" are allowed. Try again.')
-        confirm_prompt()
+        confirmation_prompt()
     answer = input("Continue? - All of your mods are being erased! [Y|N]: ")
     if answer.lower() in ["y", "yes"]:
         delete_mods()
@@ -209,12 +193,12 @@ def confirm_prompt():
         sys.exit(0)
     else:
         print('Only \"y\" or \"n\" are allowed. Try again.')
-        confirm_prompt()
+        confirmation_prompt()
 
 
 def install_mods(src):
     """Copies mods in your mods folder"""
-    mc_mod_folder = set_mod_folder()
+    mc_mod_folder = get_mod_folder(None)
     # Set + check source folder path
     if src is None:
         src = args.source
@@ -231,13 +215,35 @@ def install_mods(src):
 
 def install_mods_zip(zipfile_path):
     """Extracts a zip file containing mods to the mods folder"""
-    mc_mod_folder = set_mod_folder()
+    mc_mod_folder = get_mod_folder(None)
     # Extract zip file
     with ZipFile(zipfile_path, 'r') as zipobject:
         zipobject.extractall(
             path=mc_mod_folder)
     zipobject.close()
 
+
+# Check config.ini
+if args.destination is None:
+    if config.get('Paths', 'default-backup-path') != "default":
+        if not os.path.isdir(config.get('Paths', 'default-backup-path')):
+            print("The backup path in the configuration isn't valid or doesn't exist.")
+            sys.exit(1)
+if args.mc_directory is None:
+    if config.get('Paths', 'minecraft-mod-folder') != "default":
+        if not os.path.isdir(config.get('Paths', 'minecraft-mod-folder')):
+            print(
+                "The minecraft mod folder path in the configuration isn't valid or doesn't exist.")
+            sys.exit(1)
+if args.cache_folder is None:
+    if config.get('Paths', 'cache-folder') != "default":
+        if not os.path.isdir(config.get('Paths', 'cache-folder')):
+            print("The cache path in the configuration isn't given.")
+            sys.exit(1)
+if (config.get('General', 'operating-system') != "Windows" and
+        config.get('General', 'operating-system') != "Linux"):
+    print('Please check your configuration at operating-system.')
+    sys.exit(1)
 
 # Run selected commands
 if __name__ == "__main__":
@@ -249,7 +255,7 @@ if __name__ == "__main__":
         if args.silent:
             delete_mods()
         elif not args.silent:
-            confirm_prompt()
+            confirmation_prompt()
     elif args.command == "install":
         install_mods(None)
     else:
